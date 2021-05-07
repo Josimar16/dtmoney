3d98c1cf49169bfae29fe96a4cb78172e1b63a9f
Josimar16 <josimarjr479@gmail.com>
2021-05-07 13:58:46 -0300

    style: :lipstick: Adicionando estilização de modal no global.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { useCallback, useState } from 'react';
 import { GlobalStyle } from './styles/global';
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 export const App = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  const handleOpenNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(true);
+  }, []);
+
+  const handleCloseNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false);
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
